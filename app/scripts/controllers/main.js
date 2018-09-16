911036e5d6df2001a7834ed234afcb51a1b74ef3
SunEagle <marcosvinicci@yahoo.ie>
2018-09-15 22:33:20 -0300

    First Commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name mnFrontendTestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the mnFrontendTestApp
+ */
+angular.module('mnFrontendTestApp')
+  .controller('MainCtrl', function ($scope) {
+    
+  });
